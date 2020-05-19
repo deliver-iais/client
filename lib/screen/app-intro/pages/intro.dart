d4ecaccc2a036408682a186f2a4c76a0ee9a38b5
Fatemeh <f.soltanzadeh1378@gmail.com>
2020-05-19 19:54:17 +0430

    Some feature added and logo finded

--- lib/screen/app-intro/pages/intro.dart
+++ lib/screen/app-intro/pages/intro.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+final List<String> imgList = [
+  ];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset(item, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
+
 class IntroPage extends StatefulWidget {
   @override
   _IntroPageState createState() => _IntroPageState();
@@ -8,6 +53,7 @@ class IntroPage extends StatefulWidget {
 
 class _IntroPageState extends State<IntroPage> {
   final CarouselController _controller = CarouselController();
+  int _current = 0;
 
   @override
   void initState() {
@@ -17,13 +63,46 @@ class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child:CarouselSlider(
-          items: <Widget>[],
-          options: CarouselOptions(enlargeCenterPage: true, height: 200),
-          carouselController: _controller,
-        )
-      )
-    );
+        body: SingleChildScrollView(
+            child: Column(children: [
+      CarouselSlider(
+        items: <Widget>[],
+        options: CarouselOptions(onPageChanged: (index, reason) {
+          setState(() {
+            _current = index;
+          });
+        }),
+        carouselController: _controller,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+              RaisedButton(
+                onPressed: null,
+                child: Text("Skip"),
+              )
+            ] +
+            imgList.map((url) {
+              int index = imgList.indexOf(url);
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              );
+            }).toList() +
+            <Widget>[
+              RaisedButton(
+                onPressed: () => _controller.nextPage(),
+                child: Text("Next"),
+              )
+            ],
+      ),
+    ])));
   }
 }
