74291dc0e5e79b935e2ed6149ced7052200e959a
Fatemeh <f.soltanzadeh1378@gmail.com>
2020-07-11 14:43:32 +0430

    one models directory instead of models for every directory

--- lib/screen/splashScreen/pages/splashScreen.dart
+++ lib/screen/splashScreen/pages/splashScreen.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:deliver_flutter/routes/router.gr.dart';
-import 'package:deliver_flutter/screen/app-auth/models/loggedinStatus.dart';
+import 'package:deliver_flutter/models/loggedinStatus.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
