a7d6cf81eea2ac4d08650b42ee0d228c5b2c0acd
h.beheshti <bitbeter@gmail.com>
2020-07-20 12:53:04 +0430

    extra theme added for more theme settings and dynamic changing added too

---
+++ lib/generated/pub/v1/models/avatar.pb.dart
@@ -0,0 +1,56 @@
+///
+//  Generated code. Do not modify.
+//  source: pub/v1/models/avatar.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class Avatar extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo('Avatar', package: const $pb.PackageName('proto.pub.v1.models'), createEmptyInstance: create)
+    ..aOS(1, 'node')
+    ..aOS(2, 'category')
+    ..pPS(3, 'fileIds', protoName: 'fileIds')
+    ..hasRequiredFields = false
+  ;
+
+  Avatar._() : super();
+  factory Avatar() => create();
+  factory Avatar.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Avatar.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  Avatar clone() => Avatar()..mergeFromMessage(this);
+  Avatar copyWith(void Function(Avatar) updates) => super.copyWith((message) => updates(message as Avatar));
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Avatar create() => Avatar._();
+  Avatar createEmptyInstance() => create();
+  static $pb.PbList<Avatar> createRepeated() => $pb.PbList<Avatar>();
+  @$core.pragma('dart2js:noInline')
+  static Avatar getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Avatar>(create);
+  static Avatar _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get node => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set node($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasNode() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearNode() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get category => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set category($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasCategory() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearCategory() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $core.List<$core.String> get fileIds => $_getList(2);
+}
+
