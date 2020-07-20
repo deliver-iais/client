c35b66e76aafd63c7dcb19beb8cec2800aef2659
e.dansi <e.dansi.111@gmail.com>
2020-07-20 19:04:21 +0430

    get verification code

---
+++ lib/repository/avatarRepo.dart
@@ -0,0 +1,27 @@
+import 'package:deliver_flutter/generated-protocol/pub/v1/avatar.pbgrpc.dart';
+import 'package:deliver_flutter/generated-protocol/pub/v1/models/avatar.pb.dart';
+import 'package:deliver_flutter/generated-protocol/pub/v1/models/uid.pb.dart';
+import 'package:grpc/grpc.dart';
+
+class AvatarRepo {
+  static ClientChannel clientChannel = ClientChannel('172.16.111.171',
+      port: 30000,
+      options: ChannelOptions(credentials: ChannelCredentials.insecure()));
+  var AvatarServices = AvatarServiceClient(clientChannel);
+
+  getAvatar(List<Uid> list) {
+    var getAvatarReq = GetAvatarReq();
+    list.forEach((element) {getAvatarReq.uidList.add(element);});
+    var getAvatars = AvatarServices.getAvatar(getAvatarReq);
+    getAvatars.then((list) => {
+
+      // todo
+    }).
+    catchError((e)=>{
+
+    });
+
+
+
+  }
+}
