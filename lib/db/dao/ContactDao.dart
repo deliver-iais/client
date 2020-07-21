bf62bbcf1526204f829617fc1475d291e989bcff
e.dansi <e.dansi.111@gmail.com>
2020-07-21 17:13:19 +0430

    add FileRepo

--- lib/db/dao/ContactDao.dart
+++ lib/db/dao/ContactDao.dart
@@ -10,6 +10,10 @@ class ContactDao extends DatabaseAccessor<Database> with _$ContactDaoMixin {
 
   ContactDao(this.database) : super(database);
 
+  Future insetContact (Contact contact)=> into(contacts).insert(contact);
+
+  Future deleteAvatar (Contact contact) => delete(contacts).delete(contact);
+
   getContact(String uid) {
     return (select(contacts)..where((tbl) => tbl.uid.equals(uid))).get();
   }
--- lib/db/dao/FileDao.dart
+++ lib/db/dao/FileDao.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:moor/moor.dart';
 import '../Files.dart';
 import '../database.dart';
@@ -9,4 +11,10 @@ part 'FileDao.g.dart';
 class FileDao extends DatabaseAccessor<Database>with _$FileDaoMixin{
   final Database database;
   FileDao(this.database):super(database);
+
+  Future insetAvatar (File file)=> into(files).insert(file);
+
+  Future deleteAvatar (File file) => delete(files).delete(file);
+
+  getFile(id)=>select(files)..where((tbl) => tbl.id.equals(id));
 }
