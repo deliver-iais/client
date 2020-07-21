03374cb15b08e9aa802fea660cffddf2ece8e5bb
e.dansi <e.dansi.111@gmail.com>
2020-07-21 10:56:49 +0430

    add servicesDiscoverinfo class

--- lib/repository/servicesDiscoveryRepo.dart
+++ lib/repository/servicesDiscoveryRepo.dart
@@ -1,8 +1,14 @@
 class ServicesDiscoveryRepo {
+  ConnectionDetails AvatarConnectipon = ConnectionDetails()
+    ..host = "172.16.111.171"
+    ..port = 30000;
 
-
+  ConnectionDetails AuthConnection = ConnectionDetails()
+    ..host = " 172.16.111.171"
+    ..port = 30000;
 }
-class ConnectionDetails{
 
+class ConnectionDetails {
+  String host;
+  int port;
 }
-
