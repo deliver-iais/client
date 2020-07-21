b6282b36e36f58b1a873b0815518801f78957039
e.dansi <e.dansi.111@gmail.com>
2020-07-21 16:16:29 +0430

    agg query

--- lib/db/dao/ContactDao.dart
+++ lib/db/dao/ContactDao.dart
@@ -5,10 +5,14 @@ import '../database.dart';
 part 'ContactDao.g.dart';
 
 @UseDao(tables: [Contacts])
-
 class ContactDao extends DatabaseAccessor<Database> with _$ContactDaoMixin {
   final Database database;
+
   ContactDao(this.database) : super(database);
 
+  getContact(String uid) {
+    return (select(contacts)..where((tbl) => tbl.uid.equals(uid))).get();
+  }
 
+  Stream getAllContacts() => select(contacts).watch();
 }
