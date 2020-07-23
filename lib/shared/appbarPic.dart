c445d3568e435a6ea1de627a99d5f83e6e58129d
e.dansi <e.dansi.111@gmail.com>
2020-07-23 11:10:07 +0430

    test is ok

--- lib/shared/appbarPic.dart
+++ lib/shared/appbarPic.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:deliver_flutter/routes/router.gr.dart';
 import 'package:deliver_flutter/shared/circleAvatar.dart';
-import 'package:deliver_flutter/theme/extra_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppbarPic extends StatefulWidget {
