1cbc1840a53d11118a602e921024747e73fb6d0f
e.dansi <e.dansi.111@gmail.com>
2020-07-25 14:52:26 +0430

    send and get contacts

--- lib/repository/contactRepo.dart
+++ lib/repository/contactRepo.dart
@@ -1,10 +1,13 @@
+import 'dart:wasm';
 
 import 'package:deliver_flutter/generated-protocol/pub/v1/models/contact.pb.dart';
+import 'package:deliver_flutter/generated-protocol/pub/v1/models/phone.pb.dart';
 import 'package:deliver_flutter/generated-protocol/pub/v1/profile.pbgrpc.dart';
 import 'package:deliver_flutter/repository/servicesDiscoveryRepo.dart';
-import 'package:deliver_flutter/services/contactServices.dart';
+import 'package:contacts_service/contacts_service.dart' as PhoneContact;
 import 'package:get_it/get_it.dart';
 import 'package:grpc/grpc.dart';
+import 'package:fixnum/fixnum.dart';
 
 class ContactRepo {
   static var servicesDiscoveryRepo = GetIt.I.get<ServicesDiscoveryRepo>();
@@ -13,13 +16,32 @@ class ContactRepo {
       servicesDiscoveryRepo.ContactServies.host,
       port: servicesDiscoveryRepo.ContactServies.port,
       options: ChannelOptions(credentials: ChannelCredentials.insecure()));
+  var contactServices = ContactServiceClient(clientChannel);
 
-  sendContacts(List<Contact> list) async {
-    var contactServices = ContactServiceClient(clientChannel);
+  SyncContacts() async {
+    List<PhoneContact.Contact> phoneContacts =
+        await PhoneContact.ContactsService.getContacts();
+    List<Contact> contacts = new List(phoneContacts.length);
+    for (PhoneContact.Contact phoneContact in phoneContacts) {
+      PhoneNumber phoneNumber = PhoneNumber()
+        ..nationalNumber =
+            Int64.parseInt(phoneContact.phones.elementAt(0).toString());
+      Contact contact = Contact()
+        ..lastName = phoneContact.displayName
+        ..phoneNumber = phoneNumber;
+      contacts.add(contact);
+    }
+
     var sendContacts = SaveContactsReq();
-    list.forEach((element) {
+    contacts.forEach((element) {
       sendContacts.contactList.add(element);
     });
-   contactServices.saveContacts(sendContacts);
+    contactServices.saveContacts(sendContacts);
+    print("contacts send");
+  }
+
+
+ getContacts () async {
+      return contactServices.getContactList(GetContactListReq());
   }
 }
