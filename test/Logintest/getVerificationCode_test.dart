cb37bfe71336ba141046ac97ec1fc35164f78064
e.dansi <e.dansi.111@gmail.com>
2020-08-02 11:26:40 +0430

    save in shared not write

--- test/Logintest/getVerificationCode_test.dart
+++ test/Logintest/getVerificationCode_test.dart
@@ -3,12 +3,14 @@ import 'package:deliver_flutter/generated-protocol/pub/v1/profile.pbgrpc.dart';
 import 'package:deliver_flutter/repository/accountRepo.dart';
 import 'package:deliver_flutter/repository/profileRepo.dart';
 import 'package:deliver_flutter/repository/servicesDiscoveryRepo.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:grpc/grpc.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:fixnum/fixnum.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // test AuthServiceClient by Mock server
 
@@ -16,18 +18,17 @@ class MockAuthServiceClient extends Mock implements AuthServiceClient {}
 
 void main() {
 
+  TestWidgetsFlutterBinding.ensureInitialized();
   PhoneNumber phoneNumber = PhoneNumber()
     ..countryCode = 98
     ..nationalNumber = Int64.parseInt("9121234567");
 
-
-
   test("AuthServiceClient ", () async {
     final mockClient = MockAuthServiceClient();
 //
-  var re = new GetVerificationCodeReq()
-  ..phoneNumber=phoneNumber
-  ..type=VerificationType.SMS;
+    var re = new GetVerificationCodeReq()
+      ..phoneNumber = phoneNumber
+      ..type = VerificationType.SMS;
 
     when(mockClient.getVerificationCode(GetVerificationCodeReq()
           ..phoneNumber = phoneNumber
@@ -36,16 +37,17 @@ void main() {
       print("code is send ");
     });
 
-  var loginRequest =   mockClient.getVerificationCode(GetVerificationCodeReq()
-    ..phoneNumber=phoneNumber
-    ..type = VerificationType.SMS);
+    var loginRequest = mockClient.getVerificationCode(GetVerificationCodeReq()
+      ..phoneNumber = phoneNumber
+      ..type = VerificationType.SMS);
 
     expect(dynamic, dynamic);
-
   });
 
   test("Send Verification Code", () async {
     final mockClient = MockAuthServiceClient();
+
+ //   SharedPreferences _prefs = await SharedPreferences.getInstance();
 
     var request = VerifyCodeReq()
       ..phoneNumber = phoneNumber
@@ -53,9 +55,10 @@ void main() {
       ..code = "12345"
       ..password = "12";
 
-    when(mockClient.verifyAndGetToken(request))
-        .thenAnswer((realInvocation) {
+    when(mockClient.verifyAndGetToken(request)).thenAnswer((realInvocation) {
       print("code is verify");
+      print("accessToken and refreshToken is receved ");
+      print("save in SharedPreferences  "); // by soe error: save in shared not write.
     });
 
     var loginRequest = await mockClient.verifyAndGetToken(VerifyCodeReq()
@@ -68,4 +71,3 @@ void main() {
 
   });
 }
-
