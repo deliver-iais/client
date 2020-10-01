719f1f451b198539b576f95caa222e6024e6d811
Fatemeh <f.soltanzadeh1378@gmail.com>
2020-10-01 16:56:06 +0330

    added updating item to modeChecker and merge to message repo

--- lib/repository/messageRepo.dart
+++ lib/repository/messageRepo.dart
@@ -1,19 +1,27 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:deliver_flutter/db/dao/RoomDao.dart';
 import 'package:deliver_flutter/db/database.dart';
+import 'package:deliver_flutter/models/app_mode.dart';
 import 'package:deliver_flutter/models/messageType.dart';
 import 'package:deliver_flutter/models/sending_status.dart';
 import 'package:deliver_flutter/repository/accountRepo.dart';
 import 'package:deliver_flutter/repository/fileRepo.dart';
 import 'package:deliver_flutter/services/message_service.dart';
+import 'package:deliver_flutter/services/mode_checker.dart';
+import 'package:deliver_public_protocol/pub/v1/core.pbgrpc.dart';
 import 'package:deliver_public_protocol/pub/v1/models/uid.pb.dart';
+import 'package:deliver_public_protocol/pub/v1/models/user_room_meta.pb.dart';
+import 'package:deliver_public_protocol/pub/v1/query.pb.dart';
+import 'package:deliver_public_protocol/pub/v1/query.pbgrpc.dart';
 import 'package:fimber/fimber.dart';
 import 'package:get_it/get_it.dart';
 import 'package:deliver_flutter/db/dao/MessageDao.dart';
 import 'package:deliver_flutter/db/dao/PendingMessageDao.dart';
 import 'package:deliver_flutter/shared/extensions/uid_extension.dart';
+import 'package:grpc/grpc.dart';
 import 'package:path_provider/path_provider.dart';
 
 class MessageRepo {
@@ -23,7 +31,77 @@ class MessageRepo {
   AccountRepo accountRepo = GetIt.I.get<AccountRepo>();
   MessageService messageService = GetIt.I.get<MessageService>();
   FileRepo fileRepo = GetIt.I.get<FileRepo>();
+  ModeChecker modeChecker = GetIt.I.get<ModeChecker>();
 
+  static ClientChannel _clientChannel = ClientChannel("172.16.111.189",
+      port: 30100,
+      options: ChannelOptions(credentials: ChannelCredentials.insecure()));
+
+  QueryServiceClient _queryServiceClient = QueryServiceClient(_clientChannel);
+
+  MessageRepo() {
+    modeChecker.appMode.listen((mode) {
+      if (mode == AppMode.STABLE) {
+        updating();
+      }
+    });
+
+    // connecting
+
+    // establish();
+  }
+
+  updating() async {
+    modeChecker.updating.add(true);
+    try {
+      var getAllUserRoomMetaRes = await _queryServiceClient.getAllUserRoomMeta(
+          GetAllUserRoomMetaReq(),
+          options: CallOptions(
+              metadata: {'accessToken': await accountRepo.getAccessToken()}));
+      for (UserRoomMeta userRoomMeta in getAllUserRoomMetaRes.roomsMeta) {
+        Room room = await roomDao.getByRoomId(userRoomMeta.uid.string).single;
+        if (room.lastMessage != userRoomMeta.lastMessageId.toInt()) {
+          try {
+            var fetchMessagesRes = await _queryServiceClient.fetchMessages(
+                FetchMessagesReq()
+                  ..with_1 = room.roomId.uid
+                  ..pointer = userRoomMeta.lastMessageId
+                  ..type = FetchMessagesReq_Type.FORWARD_FETCH
+                  ..limit = 5,
+                options: CallOptions(metadata: {
+                  'accessToken': await accountRepo.getAccessToken()
+                }));
+            messageDao.insertMessage(Message(
+                roomId: room.roomId,
+                packetId: null, //?
+                id: userRoomMeta.lastMessageId.toInt() +
+                    fetchMessagesRes.messages.length -
+                    1, //?
+                time: null, //?
+                from: fetchMessagesRes.messages.last.from.string,
+                to: fetchMessagesRes.messages.last.to.string,
+                edited: fetchMessagesRes.messages.last.edited,
+                encrypted: fetchMessagesRes.messages.last.encrypted,
+                type: null, //?
+                json: null)); //?
+            await roomDao.updateRoom(room.copyWith(
+                lastMessage: userRoomMeta.lastMessageId.toInt() +
+                    fetchMessagesRes.messages.length -
+                    1));
+          } catch (e) {
+            print(e);
+          }
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+    modeChecker.updating.add(false);
+  }
+
+
+
+  reconnecting() {}
   sendTextMessage(Uid roomId, String text,
       {int replyId, String forwardedFrom}) async {
     Message message = Message(
