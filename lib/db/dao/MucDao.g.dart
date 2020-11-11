4452ac91d1b739b186a81d0bb1aab79d7e43d671
e.dansi <e.dansi.111@gmail.com>
2020-11-11 14:11:57 +0330

    merge by releze

---
+++ lib/db/dao/MucDao.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'MucDao.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$MucDaoMixin on DatabaseAccessor<Database> {
+  $MucsTable get mucs => attachedDatabase.mucs;
+}
