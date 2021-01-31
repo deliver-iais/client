79dc62a3cc8657dd9fc92f233a6b04199b926151
p.asghari <parinazasghari75@gamil.com>
2021-01-31 21:18:18 +0330

    swipe bug fixed

--- lib/screen/app-room/messageWidgets/video_message/video_ui.dart
+++ lib/screen/app-room/messageWidgets/video_message/video_ui.dart
@@ -22,6 +22,8 @@ class _VideoUiState extends State<VideoUi> {
   VideoPlayerService videoPlayerService = new VideoPlayerService();
    BehaviorSubject<bool> isPlaySubject = BehaviorSubject.seeded(false);
   BehaviorSubject<double> _currentPositionSubject ;
+  bool isPlaying=false;
+
 
   @override
   void dispose() {
@@ -50,45 +52,53 @@ class _VideoUiState extends State<VideoUi> {
           height: MediaQuery.of(context).size.height,
           child: FittedBox(
             fit: BoxFit.fitWidth,
-            child: Center(
-              child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: VideoPlayer(videoPlayerService.videoPlayerController)),
+            child: Stack(
+              children: [
+                Center(
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 2,
+                    child: VideoPlayer(videoPlayerService.videoPlayerController)),
+              ),
+                if(isPlaying==true)
+                Positioned(
+                  top: 80,
+                  left: 0.0,
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: SliderTheme(
+                    data: SliderThemeData(
+                      thumbColor: ExtraTheme.of(context).active,
+                      trackHeight: 2.25,
+                      activeTrackColor: ExtraTheme.of(context).active,
+                      inactiveTrackColor: ExtraTheme.of(context).text,
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 4.5),
+                    ),
+                    child: StreamBuilder<double>(
+                      stream: _currentPositionSubject.stream,
+                      builder: (c, s) {
+                        if (s.hasData)
+                          return Slider(
+                              value: s.data,
+                              min: 0.0,
+                              max: widget.duration,
+                              onChanged: (double value) {
+                                videoPlayerService.videoPlayerController
+                                    .seekTo(Duration(seconds: value.toInt()));
+                              });
+                        else
+                          return Container();
+                      },
+                    ),
+                  ),
+                ),
+
+              ],
             ),
           ),
         ),
 
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 180, 0, 0),
-            child: SliderTheme(
-              data: SliderThemeData(
-                thumbColor: ExtraTheme.of(context).active,
-                trackHeight: 2.25,
-                activeTrackColor: ExtraTheme.of(context).active,
-                inactiveTrackColor: ExtraTheme.of(context).text,
-                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 4.5),
-              ),
-              child: StreamBuilder<double>(
-                stream: _currentPositionSubject.stream,
-                builder: (c, s) {
-                  if (s.hasData)
-                    return Slider(
-                        value: s.data,
-                        min: 0.0,
-                        max: widget.duration,
-                        onChanged: (double value) {
-                          videoPlayerService.videoPlayerController
-                              .seekTo(Duration(seconds: value.toInt()));
-                        });
-                  else
-                    return Container();
-                },
-              ),
-            ),
-          ),
-        ),
+
         Center(
             child: StreamBuilder<bool>(
               stream: isPlaySubject.stream,
@@ -111,6 +121,9 @@ class _VideoUiState extends State<VideoUi> {
                         : IconButton(
                         icon: Icon(Icons.play_arrow),
                         onPressed: () async {
+                          setState(() {
+                            isPlaying=true;
+                          });
                           isPlaySubject.add(true);
                           videoPlayerService.videoPlayerController.play();
                         }),
