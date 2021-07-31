d1b9850207f129fcb33a71cfe0065d58bc232134
h.beheshti <bitbeter@gmail.com>
2021-07-31 15:32:45 +0430

    minor changes

--- lib/screen/room/widgets/sendedMessageBox.dart
+++ lib/screen/room/widgets/sendedMessageBox.dart
@@ -35,20 +35,32 @@ class SentMessageBox extends StatelessWidget {
             pattern: this.pattern,
           )
         : Padding(
-            padding: const EdgeInsets.only(bottom: 8.0, right: 5.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-              child: Container(
-                padding: const EdgeInsets.all(2),
-                color: ExtraTheme.of(context).sentMessageBox,
-                child: BoxContent(
-                  message: message,
-                  maxWidth: maxWidth,
-                  isSender: true,
-                  scrollToMessage: scrollToMessage,
-                  isSeen: this.isSeen,
-                  pattern: pattern,
-                  onUsernameClick: this.omUsernameClick,
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+                      .copyWith(bottomRight: Radius.zero),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 0.5,
+                      offset: Offset(0, 0.5), // Shadow position
+                    ),
+                  ]),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(8))
+                    .copyWith(bottomRight: Radius.zero),
+                child: Container(
+                  color: ExtraTheme.of(context).sentMessageBox,
+                  child: BoxContent(
+                    message: message,
+                    maxWidth: maxWidth,
+                    isSender: true,
+                    scrollToMessage: scrollToMessage,
+                    isSeen: this.isSeen,
+                    pattern: pattern,
+                    onUsernameClick: this.omUsernameClick,
+                  ),
                 ),
               ),
             ),
