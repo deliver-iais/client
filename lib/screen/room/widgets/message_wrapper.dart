63a89637ca52225c6ca60c878072c576998d972c
hasan <bitbeter@gmail.com>
2021-09-08 14:32:45 +0430

    improve ui

--- lib/screen/room/widgets/message_wrapper.dart
+++ lib/screen/room/widgets/message_wrapper.dart
@@ -11,21 +11,69 @@ class MessageWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     const radius = const Radius.circular(12);
     const border = const BorderRadius.all(radius);
-    return Padding(
+    return Container(
       padding: const EdgeInsets.all(4.0),
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
       child: Container(
         decoration: BoxDecoration(borderRadius: border, boxShadow: [
           BoxShadow(
               color: Colors.black38, blurRadius: 0.5, offset: Offset(0, 0.5))
         ]),
-        child: ClipRRect(
-            borderRadius: border,
-            child: Container(
-                color: isSent
-                    ? ExtraTheme.of(context).sentMessageBox
-                    : ExtraTheme.of(context).receivedMessageBox,
-                child: child)),
+        child: Stack(
+          children: [
+            Positioned(
+              left: isSent ? null : 0,
+              right: !isSent ? null : 0,
+              top: 0,
+              child: Container(
+                  // color: Colors.white,
+                  width: 20,
+                  height: 20,
+                  child: CustomPaint(
+                    foregroundPainter: OPainter(
+                        isSent
+                            ? ExtraTheme.of(context).sentMessageBox
+                            : ExtraTheme.of(context).receivedMessageBox,
+                        isSent),
+                  )),
+            ),
+            ClipRRect(
+                borderRadius: border,
+                child: Container(
+                    color: isSent
+                        ? ExtraTheme.of(context).sentMessageBox
+                        : ExtraTheme.of(context).receivedMessageBox,
+                    child: child)),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class OPainter extends CustomPainter {
+  final Color color;
+  final bool isSent;
+
+  OPainter(this.color, this.isSent);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 2;
+    //draw arc
+    canvas.drawArc(
+        Offset(isSent ? 6 : -12, 4) & Size(size.width * 1.3, size.height * 1.2),
+        isSent ? -3 : -2, //radians
+        isSent ? 2 : 2, //radians
+        true,
+        paint1);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
