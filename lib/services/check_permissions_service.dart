a3b24da33a87138b594756bdca3a5bb775a4dd1a
e.dansi <e.dansi.111@gmail.com>
2021-09-19 11:27:46 +0430

    some bug fix

--- lib/services/check_permissions_service.dart
+++ lib/services/check_permissions_service.dart
@@ -7,38 +7,54 @@ class CheckPermissionsService {
 
 extension PermissionsExtension on CheckPermissionsService {
   Future<bool> checkContactPermission() async {
-    if (!await Permission.contacts.isGranted) {
-      return await requestLock.synchronized(() async {
-        return await Permission.contacts.request().isGranted;
-      });
-    } else {
-      return true;
+    try {
+      if (!await Permission.contacts.isGranted) {
+        return await requestLock.synchronized(() async {
+          return await Permission.contacts.request().isGranted;
+        });
+      } else {
+        return true;
+      }
+    } catch (e) {
+      return false;
     }
   }
 
   Future<bool> checkAudioRecorderPermission() async {
-    if (!await Permission.microphone.isGranted) {
-      return await Permission.microphone.request().isGranted;
-    } else {
-      return true;
+    try {
+      if (!await Permission.microphone.isGranted) {
+        return await Permission.microphone.request().isGranted;
+      } else {
+        return true;
+      }
+    } catch (e) {
+      return false;
     }
   }
 
   Future<bool> checkLocationPermission() async {
-    if (!await Permission.location.isGranted) {
-      return await Permission.location.request().isGranted;
-    } else {
-      return true;
+    try {
+      if (!await Permission.location.isGranted) {
+        return await Permission.location.request().isGranted;
+      } else {
+        return true;
+      }
+    } catch (e) {
+      return false;
     }
   }
 
   Future<bool> checkStoragePermission() async {
-    if (!await Permission.accessMediaLocation.isGranted) {
-      return await requestLock.synchronized(() async {
-        return await Permission.accessMediaLocation.request().isGranted;
-      });
-    } else {
-      return true;
+    try {
+      if (!await Permission.accessMediaLocation.isGranted) {
+        return await requestLock.synchronized(() async {
+          return await Permission.accessMediaLocation.request().isGranted;
+        });
+      } else {
+        return true;
+      }
+    } catch (e) {
+      return false;
     }
   }
 }
