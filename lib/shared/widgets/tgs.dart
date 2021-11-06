ab74ba42db19c51bb12974a280d148f9c1456aa3
h.beheshti <bitbeter@gmail.com>
2021-11-06 18:43:01 +0330

    minor change in UI

--- lib/shared/widgets/tgs.dart
+++ lib/shared/widgets/tgs.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 
 class TGSController {
   final bool repeat;
@@ -15,22 +19,60 @@ class TGSController {
     ctrl.dispose();
   }
 
-  void animate() {}
+  void animate() {
+    ctrl.forward(from: 0);
+  }
 }
 
 class TGS extends StatefulWidget {
   final TGSController controller;
 
-  TGS({Key key, this.controller}) : super(key: key);
+  final File file;
+  final String assetsPath;
+
+  TGS.assets(this.assetsPath, {Key key, TGSController controller})
+      : file = null,
+        controller = controller ?? TGSController(),
+        super(key: key);
+
+  TGS.file(this.file, {Key key, TGSController controller})
+      : assetsPath = null,
+        controller = controller ?? TGSController(),
+        super(key: key);
 
   @override
   _TGSState createState() => _TGSState();
 }
 
 class _TGSState extends State<TGS> with TickerProviderStateMixin {
+  Future<LottieComposition> _composition;
+
+  Future<LottieComposition> _loadAssetsComposition() async {
+    var assetData = await rootBundle.load(widget.assetsPath);
+
+    var bytes = assetData.buffer.asUint8List();
+
+    bytes = GZipCodec().decode(bytes);
+
+    return await LottieComposition.fromBytes(bytes);
+  }
+
+  Future<LottieComposition> _loadFileComposition() async {
+    var bytes = await widget.file.readAsBytes();
+
+    bytes = GZipCodec().decode(bytes);
+
+    return await LottieComposition.fromBytes(bytes);
+  }
+
   @override
   void initState() {
     widget.controller.init(this);
+    if (widget.assetsPath != null) {
+      _composition = _loadAssetsComposition();
+    } else {
+      _composition = _loadFileComposition();
+    }
     super.initState();
   }
 
