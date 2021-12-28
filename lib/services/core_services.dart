ff3ee54719ce0f5ef94c0741d5adc64549b5fb31
E Dansi <e.dansi.111@gmail.com>
2021-12-28 17:29:56 +0330

    some bug fix

--- lib/services/core_services.dart
+++ lib/services/core_services.dart
@@ -200,7 +200,7 @@ class CoreServices {
         ..message = message
         ..id = DateTime.now().microsecondsSinceEpoch.toString();
       _sendPacket(clientPacket);
-      Timer(const Duration(seconds: MIN_BACKOFF_TIME ~/ 2),
+      Timer(Duration(seconds: MIN_BACKOFF_TIME ~/ 2),
           () => _checkPendingStatus(message.packetId));
     } catch (e) {
       _logger.e(e);
