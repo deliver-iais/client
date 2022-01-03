686f1321dc21569590ee0cc9c35f3145e5862dbd
Fatemeh Chitsaz <f.chitsaz7919@gmail.com>
2022-01-03 15:23:21 +0330

    register services

--- test/helper/test_helper.dart
+++ test/helper/test_helper.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:deliver/box/dao/block_dao.dart';
 import 'package:deliver/box/dao/message_dao.dart';
 import 'package:deliver/box/dao/room_dao.dart';
@@ -12,6 +11,7 @@ import 'package:deliver/services/core_services.dart';
 import 'package:deliver/services/muc_services.dart';
 import 'package:deliver_public_protocol/pub/v1/query.pbgrpc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:mockito/annotations.dart';
 import '../helper/test_helper.mocks.dart';
 
@@ -41,13 +41,68 @@ MockCoreServices getAndRegisterCoreServices() {
 MockLogger getAndRegisterLogger() {
   _removeRegistrationIfExists<Logger>();
   final service = MockLogger();
-  GetIt.I.registerSingleton<Logger>(service);
+  GetIt.I.registerSingleton<Logger>(Logger());
+  GetIt.I.registerSingleton<MockLogger>(service);
+  return service;
+}
+
+MockMessageDao getAndRegisterMessageDao() {
+  _removeRegistrationIfExists<MessageDao>();
+  final service = MockMessageDao();
+  GetIt.I.registerSingleton<MessageDao>(service);
+  return service;
+}
+
+MockRoomDao getAndRegisterRoomDao() {
+  _removeRegistrationIfExists<RoomDao>();
+  final service = MockRoomDao();
+  GetIt.I.registerSingleton<RoomDao>(service);
+  return service;
+}
+
+MockRoomRepo getAndRegisterRoomRepo() {
+  _removeRegistrationIfExists<RoomRepo>();
+  final service = MockRoomRepo();
+  GetIt.I.registerSingleton<RoomRepo>(service);
+  return service;
+}
+
+MockAuthRepo getAndRegisterAuthRepo() {
+  _removeRegistrationIfExists<AuthRepo>();
+  final service = MockAuthRepo();
+  GetIt.I.registerSingleton<AuthRepo>(service);
+  return service;
+}
+
+MockFileRepo getAndRegisterFileRepo() {
+  _removeRegistrationIfExists<FileRepo>();
+  final service = MockFileRepo();
+  GetIt.I.registerSingleton<FileRepo>(service);
+  return service;
+}
+MockLiveLocationRepo getAndRegisterLiveLocationRepo() {
+  _removeRegistrationIfExists<LiveLocationRepo>();
+  final service = MockLiveLocationRepo();
+  GetIt.I.registerSingleton<LiveLocationRepo>(service);
+  return service;
+}
+MockSeenDao getAndRegisterSeenDao() {
+  _removeRegistrationIfExists<SeenDao>();
+  final service = MockSeenDao();
+  GetIt.I.registerSingleton<SeenDao>(service);
   return service;
 }
 
 void registerServices() {
   getAndRegisterCoreServices();
   getAndRegisterLogger();
+  getAndRegisterMessageDao();
+  getAndRegisterRoomDao();
+  getAndRegisterRoomRepo();
+  getAndRegisterAuthRepo();
+  getAndRegisterFileRepo();
+  getAndRegisterLiveLocationRepo();
+  getAndRegisterSeenDao();
 }
 
 void unregisterServices() {
