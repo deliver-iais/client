b2640fd9d58dcc26021ca3aa952fd785f2e01f55
hasan <bitbeter@gmail.com>
2022-01-29 06:49:38 +0330

    minor changes

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -89,7 +89,7 @@ const double FLUID_MAX_HEIGHT = 540;
 const double FLUID_CONTAINER_MAX_WIDTH = 768;
 const double BREAKDOWN_SIZE = 768;
 
-const double NAVIGATION_PANEL_SIZE = 342;
+const double NAVIGATION_PANEL_SIZE = 352;
 const double MIN_WIDTH = 200;
 
 //FEATURE DISCOVERY ID
