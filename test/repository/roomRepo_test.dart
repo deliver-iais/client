a31227bfe6a714f4c39665dbbcfe9224a660d250
Fatemeh Chitsaz <f.chitsaz7919@gmail.com>
2022-03-02 09:16:25 +0330

    add some tests

--- test/repository/roomRepo_test.dart
+++ test/repository/roomRepo_test.dart
@@ -41,43 +41,50 @@ void main() {
     });
     group('isVerified -', () {
       test(
-          'When called should return uid.isSystem() || (uid.isBot() && uid.node == "father_bot"',
+          'When called if category is not be System or bot and node not be father_bot should return false',
           () async {
         expect(await RoomRepo().isVerified(testUid), false);
       });
       test(
-          'When called should return uid.isSystem() || (uid.isBot() && uid.node == "father_bot"',
+          'When called if category is System or bot and node be father_bot should return true',
           () async {
         expect(await RoomRepo().isVerified(botUid), true);
       });
     });
     group('fastForwardName -', () {
-      test('When called if name is in roomNameCache should return name',
+      test('When called if name is not in roomNameCache should return null',
           () async {
         expect(RoomRepo().fastForwardName(testUid), null);
       });
-      //todo add another test when getName test pass
+      test('When called if name is in roomNameCache should return name',
+          () async {
+        //set to cache
+        roomNameCache.set(testUid.asString(), "test");
+        expect(RoomRepo().fastForwardName(testUid), "test");
+      });
     });
     group('getName -', () {
       test('When called if uid is emptyUid should return ""', () async {
         expect(await RoomRepo().getName(emptyUid), "");
       });
-      test('When called if category is SYSTEM should return ""', () async {
+      test('When called if category is SYSTEM should return APPLICATION_NAME',
+          () async {
         expect(await RoomRepo().getName(systemUid), APPLICATION_NAME);
       });
-      test('When called if uid is isCurrentUser should return accountRepo.getName', () async {
+      test(
+          'When called if uid is isCurrentUser should return accountRepo.getName',
+          () async {
         final accountRepo = getAndRegisterAccountRepo();
         getAndRegisterAuthRepo(isCurrentUser: true);
-        var name=await RoomRepo().getName(testUid);
+        var name = await RoomRepo().getName(testUid);
         verify(accountRepo.getName());
-        expect(name,"test");
+        expect(name, "test");
       });
-      test('When called if uid is isCurrentUser should return accountRepo.getName', () async {
-        final accountRepo = getAndRegisterAccountRepo();
-        getAndRegisterAuthRepo(isCurrentUser: true);
-        var name=await RoomRepo().getName(testUid);
-        verify(accountRepo.getName());
-        expect(name,"test");
+      test('When called if name is in cache should return name', () async {
+        //set to cache
+        roomNameCache.set(testUid.asString(), "test");
+        var name = await RoomRepo().getName(testUid);
+        expect(name, "test");
       });
     });
   });
