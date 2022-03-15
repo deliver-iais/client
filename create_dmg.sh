76ddf7b2ce9d2a519abda8e84e79e99502d6710d
hasan <bitbeter@gmail.com>
2022-03-15 17:55:31 +0330

    minor change

--- create_dmg.sh
+++ create_dmg.sh
@@ -2,6 +2,8 @@ flutter build macos --release
 
 rm installer/dmg_creator/deliver.dmg
 
+mv build/macos/Build/Products/Release/Deliver.app build/macos/Build/Products/Release/We.app
+
 cd installer/dmg_creator
 
-npx appdmg ./config.json ./deliver.dmg
+npx appdmg ./config.json ./we.dmg
