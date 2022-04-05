cb8a54c00ead9fda1f66e3ce4fabf495a12d21fa
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-04-05 16:15:50 +0430

    add background animation

--- lib/screen/call/audioCallScreen/fade_audio_call_background.dart
+++ lib/screen/call/audioCallScreen/fade_audio_call_background.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
 
-class FadeAudioCallBackground extends StatelessWidget {
+class FadeAudioCallBackground extends StatefulWidget {
   final ImageProvider image;
 
   const FadeAudioCallBackground({Key? key, required this.image})
       : super(key: key);
 
   @override
+  State<FadeAudioCallBackground> createState() =>
+      _FadeAudioCallBackgroundState();
+}
+
+class _FadeAudioCallBackgroundState extends State<FadeAudioCallBackground> {
+  List<double> gradientRadius = [
+    0.4,
+    0.6,
+    0.9,
+  ];
+  List<Alignment> alignmentList = [
+    Alignment.topRight,
+    Alignment.topCenter,
+    Alignment.topLeft,
+  ];
+  double radius = 0.0;
+  Alignment center = Alignment.topCenter;
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(seconds: 5),
+      onEnd: () {
+        setState(() {
+          index = index + 1;
+          radius = gradientRadius[index % gradientRadius.length];
+          center = alignmentList[index % alignmentList.length];
+        });
+      },
       decoration: BoxDecoration(
-        color:
-            Color.alphaBlend(Colors.black54, theme.primaryColor.withAlpha(150)),
+        color: Color.alphaBlend(Colors.black54, theme.primaryColor),
         image: DecorationImage(
           fit: BoxFit.cover,
           colorFilter: ColorFilter.mode(
-            Colors.black.withOpacity(0.06),
+            Colors.black.withOpacity(0.1),
             BlendMode.dstATop,
           ),
-          image: image,
+          image: widget.image,
+        ),
+        gradient: RadialGradient(
+          radius: radius,
+          center: center,
+          colors: [
+            Color.alphaBlend(
+              theme.primaryColorLight,
+              theme.scaffoldBackgroundColor.withAlpha(150),
+            ),
+            Color.alphaBlend(Colors.black54, theme.primaryColor.withAlpha(200)),
+          ],
         ),
       ),
     );
