2f201259ca53fd66cb7b662a0a7d644e3048423e
hasan <bitbeter@gmail.com>
2022-04-07 13:54:41 +0430

    some fixes

--- lib/shared/changelog.dart
+++ lib/shared/changelog.dart
@@ -1,16 +1,19 @@
 // ignore_for_file: constant_identifier_names
-//v1.6.6
+
 const List<String> FARSI_FEATURE_LIST = [
-  '1.افزودن قابلیت ویرایش عکس ',
-  '2.افزودن قابیلت کپی پیس کردن فایل ها ',
-  '3.بهبود ظاهر اپ وافزودن قابیلت انتخاب رنگ اصلی.',
-  'رفع دیگر مشکلات گزارش شده'
+  '۱. افزودن قابلیت ویرایش عکس',
+  '۲. افزودن قابیلت کپی پیس کردن فایل ها',
+  '۳. رفع باگ در scrollbar در صفحه پیام ها',
+  '۳. اضافه کردن Auto Night Mode در قسمت تنظیمات',
+  '۴. بهبود ظاهر اپ وافزودن قابیلت انتخاب رنگ اصلی در قسمت تنظیمات',
+  'رفع دیگر مشکلات گزارش شده...'
 ];
+
 const List<String> ENGLISH_FEATURE_LIST = [
-  '1.Add  edit image in all platform .',
-  '2.Add copy paste print screen in desktop',
-  '3.Auto night mode in android',
-  '4.Message scrollbar bug fix ',
-  '5.improve ui and add main color',
-  '6.some bugs fix.'
+  '1.Add edit image in all platform .',
+  '2.Add capability of "Copy & paste" all files in app including print screen clipboard in desktops application',
+  '3.Bug fix in room page scrollbar',
+  '4.Auto night mode in all platforms',
+  '5.Improve UI and adding choosing main colors in theme settings',
+  'Some bugs fix...'
 ];
