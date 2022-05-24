0c33f3b08b7faa490254ec9b1f9415e95b2a28ad
fatemeh chitsaz <f.chitsaz7918@gmail.com>
2022-05-24 14:12:12 +0430

    add saved message to the top of forward result

--- lib/screen/room/messageWidgets/forward_widgets/chat_item_to_forward.dart
+++ lib/screen/room/messageWidgets/forward_widgets/chat_item_to_forward.dart
@@ -1,4 +1,7 @@
+import 'package:deliver/localization/i18n.dart';
+import 'package:deliver/repository/authRepo.dart';
 import 'package:deliver/repository/roomRepo.dart';
+import 'package:deliver/shared/extensions/uid_extension.dart';
 import 'package:deliver/shared/widgets/circle_avatar.dart';
 import 'package:deliver_public_protocol/pub/v1/models/uid.pb.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +14,8 @@ class ChatItemToForward extends StatelessWidget {
   ChatItemToForward({Key? key, required this.uid, required this.send})
       : super(key: key);
   final _roomRepo = GetIt.I.get<RoomRepo>();
+  final _authRepo = GetIt.I.get<AuthRepo>();
+  final _i18n = GetIt.I.get<I18N>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,29 +29,33 @@ class ChatItemToForward extends StatelessWidget {
               const SizedBox(
                 width: 12,
               ),
-              CircleAvatarWidget(uid, 30),
+              CircleAvatarWidget(uid, 30, showSavedMessageLogoIfNeeded: true),
               // ContactPic(true, uid),
               const SizedBox(
                 width: 12,
               ),
-              FutureBuilder<String>(
-                future: _roomRepo.getName(uid),
-                builder: (c, snaps) {
-                  if (snaps.hasData && snaps.data != null) {
-                    return Text(
-                      snaps.data!,
-                      style: const TextStyle(fontSize: 18),
-                    );
-                  } else {
-                    return const Text(
-                      "Unknown",
-                      style: TextStyle(fontSize: 18),
-                    );
-                  }
-                },
+              Flexible(
+                child: FutureBuilder<String>(
+                  future: _roomRepo.getName(uid),
+                  builder: (c, snaps) {
+                    if (snaps.hasData && snaps.data != null) {
+                      return Text(
+                        _authRepo.isCurrentUser(uid.asString())
+                            ? _i18n.get("saved_message")
+                            : snaps.data!,
+                        style: const TextStyle(fontSize: 18),
+                        overflow:TextOverflow.ellipsis ,
+                      );
+                    } else {
+                      return const Text(
+                        "Unknown",
+                        style: TextStyle(fontSize: 18),
+                      );
+                    }
+                  },
+                ),
               ),
 
-              const Spacer(),
             ],
           ),
         ),
