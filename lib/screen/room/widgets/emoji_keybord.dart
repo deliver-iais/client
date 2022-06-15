0d169ac56bf72ff35d475b72c7c6e0dccf93df9d
hasan <bitbeter@gmail.com>
2022-06-15 17:30:40 +0430

    minor change

--- lib/screen/room/widgets/emoji_keybord.dart
+++ lib/screen/room/widgets/emoji_keybord.dart
@@ -4,7 +4,6 @@ import 'package:deliver/shared/constants.dart';
 import 'package:deliver/shared/emoji.dart';
 import 'package:deliver/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class EmojiKeyboard extends StatefulWidget {
