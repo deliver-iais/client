c2891cf72224f19255c88bd0b64dbacb33b5264c
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-06-18 18:00:19 +0430

    add call to feature_discovery

---
+++ lib/screen/navigation_center/widgets/feature_discovery_description_widget.dart
@@ -0,0 +1,53 @@
+import 'package:feature_discovery/feature_discovery.dart';
+import 'package:flutter/material.dart';
+
+class FeatureDiscoveryDescriptionWidget extends StatelessWidget {
+  final String description;
+  final Widget? permissionWidget;
+
+  const FeatureDiscoveryDescriptionWidget({
+    super.key,
+    required this.description,
+    this.permissionWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      children: [
+        Text(description),
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () async =>
+                      FeatureDiscovery.completeCurrentStep(context),
+                  child: Text(
+                    'Understood',
+                    style:
+                        theme.textTheme.button!.copyWith(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => FeatureDiscovery.dismissAll(context),
+                  child: Text(
+                    'Dismiss',
+                    style:
+                        theme.textTheme.button!.copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            permissionWidget ?? const SizedBox.shrink(),
+          ],
+        ),
+      ],
+    );
+  }
+}
