63c13cc028f3dd2104d12fab7604442f9d9fa8ba
hasan <bitbeter@gmail.com>
2022-06-23 12:49:17 +0430

    minor changes

--- lib/screen/muc/pages/member_selection_page.dart
+++ lib/screen/muc/pages/member_selection_page.dart
@@ -29,46 +29,41 @@ class MemberSelectionPage extends StatelessWidget {
         preferredSize: const Size.fromHeight(60.0),
         child: AppBar(
           leading: _routingService.backButtonLeading(),
-          title: Row(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (mucUid != null)
-                    FutureBuilder<String?>(
-                      future: _roomRepo.getName(mucUid!),
-                      builder: (context, snapshot) {
-                        if (snapshot.data != null) {
-                          return Text(snapshot.data!);
-                        } else {
-                          return Text(_i18n.get("add_member"));
-                        }
-                      },
-                    )
-                  else
-                    Text(
-                      isChannel
-                          ? _i18n.get("newChannel")
-                          : _i18n.get("newGroup"),
-                    ),
-                  StreamBuilder<int>(
-                    stream: _createMucService.selectedLengthStream(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const SizedBox.shrink();
-                      }
-                      final members = snapshot.data!;
-                      return Text(
-                        members >= 1
-                            ? '$members ${_i18n.get("of_max_member")}'
-                            : _i18n.get("max_member"),
-                        style: theme.textTheme.subtitle2,
-                      );
-                    },
-                  )
-                ],
-              ),
-              SyncContact.syncingStatusWidget(context),
+              if (mucUid != null)
+                FutureBuilder<String?>(
+                  future: _roomRepo.getName(mucUid!),
+                  builder: (context, snapshot) {
+                    if (snapshot.data != null) {
+                      return Text(snapshot.data!);
+                    } else {
+                      return Text(_i18n.get("add_member"));
+                    }
+                  },
+                )
+              else
+                Text(
+                  isChannel
+                      ? _i18n.get("newChannel")
+                      : _i18n.get("newGroup"),
+                ),
+              StreamBuilder<int>(
+                stream: _createMucService.selectedLengthStream(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox.shrink();
+                  }
+                  final members = snapshot.data!;
+                  return Text(
+                    members >= 1
+                        ? '$members ${_i18n.get("of_max_member")}'
+                        : _i18n.get("max_member"),
+                    style: theme.textTheme.subtitle2,
+                  );
+                },
+              )
             ],
           ),
         ),
