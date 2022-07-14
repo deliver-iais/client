1420f5c5ad62b69070331e95b945b881b11017d3
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-07-15 00:46:36 +0430

    parse TextField text

--- lib/screen/room/messageWidgets/input_message_text_controller.dart
+++ lib/screen/room/messageWidgets/input_message_text_controller.dart
@@ -1,7 +1,7 @@
 import 'package:deliver/shared/parsers/detectors.dart';
 import 'package:deliver/shared/parsers/parsers.dart';
 import 'package:deliver/shared/parsers/transformers.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class InputMessageTextController extends TextEditingController {
   @override
@@ -10,10 +10,16 @@ class InputMessageTextController extends TextEditingController {
     TextStyle? style,
     required bool withComposing,
   }) {
-    final spans = onePath(
-      [Block(text: text, features: {})],
-      [emojiDetector()],
-      emojiTransformer(),
+    final spans = onePathTransform(
+      onePathMultiDetection(
+        [Block(text: text, features: {})],
+        detectorsWithSearchTermDetector(),
+       forceToDeleteReplaceFunctions: true,
+      ),
+      inlineSpanTransformer(
+        defaultColor: Theme.of(context).colorScheme.onPrimaryContainer,
+        linkColor: Theme.of(context).colorScheme.primary,
+      ),
     );
 
     return TextSpan(
