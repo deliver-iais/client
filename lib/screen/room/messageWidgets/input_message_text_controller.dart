19b182b3b4b894168dc805ad74e84eb0501a58fd
Fatemeh Chitsaz <f.chitsaz7919@gmail.com>
2022-07-22 16:32:13 +0430

    parsing if mark down enable

--- lib/screen/room/messageWidgets/input_message_text_controller.dart
+++ lib/screen/room/messageWidgets/input_message_text_controller.dart
@@ -4,21 +4,25 @@ import 'package:deliver/shared/parsers/transformers.dart';
 import 'package:flutter/material.dart';
 
 class InputMessageTextController extends TextEditingController {
+  bool isMarkDownEnable = false;
+
   @override
   TextSpan buildTextSpan({
     required BuildContext context,
     TextStyle? style,
     required bool withComposing,
   }) {
-    final spans = onePath(
-      [Block(text: text, features: {})],
-      inputTextDetectors(),
-      inlineSpanTransformer(
-        defaultColor: Theme.of(context).colorScheme.onSurface,
-        linkColor: Theme.of(context).colorScheme.primary,
-        justHighlightSpoilers: true,
-      ),
-    );
+    final spans = isMarkDownEnable
+        ? onePath(
+            [Block(text: text, features: {})],
+            inputTextDetectors(),
+            inlineSpanTransformer(
+              defaultColor: Theme.of(context).colorScheme.onSurface,
+              linkColor: Theme.of(context).colorScheme.primary,
+              justHighlightSpoilers: true,
+            ),
+          )
+        : [TextSpan(text: text)];
 
     return TextSpan(
       style: style,
