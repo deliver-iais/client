b962d0b8576b9081695b07383fee199fd4b4b79e
hasan <bitbeter@gmail.com>
2022-07-28 14:31:24 +0430

    minor change

--- integration_test/we_test.dart
+++ integration_test/we_test.dart
@@ -1,6 +1,5 @@
 import 'package:deliver/main.dart' as app;
 import 'package:deliver/screen/register/widgets/intl_phone_field.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
