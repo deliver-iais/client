34fcbe6eca1b033e3bd4132b6a5556342a221f82
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-09-13 15:58:53 +0430

    change call background

--- lib/shared/widgets/animated_gradient.dart
+++ lib/shared/widgets/animated_gradient.dart
@@ -1,88 +1,113 @@
-import 'dart:async';
+import 'dart:ui';
 
+import 'package:deliver/services/ux_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
 class AnimatedGradient extends StatefulWidget {
-  final bool isAlignmentGradiant;
   final bool isConnected;
 
   const AnimatedGradient({
     super.key,
-    required this.isConnected,
-    this.isAlignmentGradiant = false,
+    this.isConnected = false,
   });
 
   @override
   AnimatedGradientState createState() => AnimatedGradientState();
 }
 
-class AnimatedGradientState extends State<AnimatedGradient> {
-  List<Color> colorList = [
-    Colors.teal,
-    Colors.cyan,
-    Colors.cyanAccent,
-    Colors.lightBlue,
-  ];
-
-  List<Color> colorListConnected = [
-    const Color.fromARGB(255, 75, 105, 100),
-    const Color.fromARGB(255, 45, 83, 110),
-    const Color.fromARGB(255, 49, 89, 107),
-  ];
-
-  int index = 0;
-  int indexConnected = 0;
-  Color bottomColor = Colors.teal;
-  Color midColor = Colors.cyan;
-  Color topColor = Colors.greenAccent;
-  Timer? gradiantTimer;
+class AnimatedGradientState extends State<AnimatedGradient>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animationController;
 
   @override
   void dispose() {
+    _animationController.dispose();
     super.dispose();
-    gradiantTimer!.cancel();
+  }
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _animation =
+        Tween<double>(begin: 10, end: 50).animate(_animationController);
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController
+          ..reset()
+          ..forward();
+      }
+    });
+
+    _animationController.forward();
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    gradiantTimer = Timer(const Duration(milliseconds: 10), () {
-      setState(() {
-        bottomColor = const Color.fromARGB(255, 49, 89, 107);
-      });
-    });
-    return Scaffold(
-      body: Stack(
-        children: [
-          AnimatedContainer(
-            duration: const Duration(seconds: 2),
-            onEnd: () {
-              if (widget.isConnected) {
-                setState(() {
-                  indexConnected = indexConnected + 1;
-                  // animate the color
-                  bottomColor = colorListConnected[indexConnected % colorListConnected.length];
-                  midColor = colorListConnected[(indexConnected + 1) % colorListConnected.length];
-                  topColor = colorListConnected[(indexConnected + 2) % colorListConnected.length];
-                });
-              } else {
-                setState(() {
-                  index = index + 1;
-                  // animate the color
-                  bottomColor = colorList[index % colorList.length];
-                  midColor = colorList[(index + 1) % colorList.length];
-                  topColor = colorList[(index + 2) % colorList.length];
-                });
-              }
-            },
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight,
-                colors: [bottomColor, midColor, topColor],
+    return AnimatedBackground(
+      isConnected: widget.isConnected,
+      animation: _animation,
+    );
+  }
+}
+
+class AnimatedBackground extends AnimatedWidget {
+  static final uxService = GetIt.I.get<UxService>();
+  final bool isConnected;
+
+  const AnimatedBackground({
+    super.key,
+    required this.isConnected,
+    required Animation<double> animation,
+  }) : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    final gradientColors = <Color>[
+      Color(
+        uxService.getCorePalette().primary.get(uxService.themeIsDark ? 50 : 70),
+      ),
+      Color(
+        uxService
+            .getCorePalette()
+            .tertiary
+            .get(uxService.themeIsDark ? 50 : 70),
+      ),
+    ];
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        systemNavigationBarColor: isConnected
+            ? Color.alphaBlend(Colors.black12, gradientColors[1])
+            : gradientColors[1],
+        systemNavigationBarIconBrightness:
+            isConnected ? Brightness.light : null,
+      ),
+      child: Scaffold(
+        backgroundColor: gradientColors[1],
+        body: Stack(
+          children: [
+            Container(
+              height:
+                  MediaQuery.of(context).size.height * (animation.value / 100),
+              color: gradientColors[0].withOpacity(0.9),
+            ),
+            Transform.translate(
+              offset: const Offset(0, 0),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 80.0, sigmaY: 80.0),
+                child: Container(color: Colors.white.withOpacity(0)),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
