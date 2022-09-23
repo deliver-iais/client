c3958ee80561c72cec368bf0d3682db80d101fe3
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-09-23 13:17:50 +0330

    some changes

--- lib/repository/show_case_repo.dart
+++ lib/repository/show_case_repo.dart
@@ -3,7 +3,6 @@ import 'package:deliver/box/show_case.dart';
 import 'package:deliver/shared/extensions/uid_extension.dart';
 import 'package:deliver_public_protocol/pub/v1/models/file.pb.dart';
 import 'package:deliver_public_protocol/pub/v1/models/showcase.pb.dart';
-import 'package:fixnum/fixnum.dart';
 import 'package:get_it/get_it.dart';
 
 class ShowCaseRepo {
@@ -38,14 +37,14 @@ class ShowCaseRepo {
         groupedRooms: GroupedRooms(
           name: "پیشنهادی های شما",
           roomsList: [
-            RoomCase(uid: "2:85afbdc5-b6f0-405e-beda-58bb27b28fda".asUid()),
-            RoomCase(uid: "2:4c783547-3d10-404c-b82b-1486927fcf37".asUid()),
-            RoomCase(uid: "2:2c6abdb2-8a6a-4a57-9dac-9aacf936835c".asUid()),
-            RoomCase(uid: "2:5cb0f1d3-0861-41e7-a741-9bc2ba08a88b".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f6ed3557-94f5-439a-b140-0fae522dfc5d".asUid()),
-            RoomCase(uid: "2:d84dcfee-49db-4f39-962f-dc04e62037c8".asUid()),
+            RoomCase(uid: "3:a7c9605b-79cd-4764-a235-65edcecc6b2e".asUid()),
+            RoomCase(uid: "3:5b53a194-f9ea-48c4-9653-bab6f8b637ff".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
+            RoomCase(uid: "3:c123a6b0-57b7-45aa-8bb4-2b9d4ac273bf".asUid()),
+            RoomCase(uid: "3:95e40d70-6586-4662-aba3-ac17d3ca8faf".asUid()),
+            RoomCase(uid: "3:d0800022-fc53-4d53-9b42-74fa1181366f".asUid()),
+            RoomCase(uid: "3:4b76d77a-0b29-4aa5-b042-44193b6662bd".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
           ],
         ),
       ),
@@ -62,14 +61,14 @@ class ShowCaseRepo {
         groupedRooms: GroupedRooms(
           name: "جدیدترین ها",
           roomsList: [
-            RoomCase(uid: "2:4c783547-3d10-404c-b82b-1486927fcf37".asUid()),
-            RoomCase(uid: "2:2c6abdb2-8a6a-4a57-9dac-9aacf936835c".asUid()),
-            RoomCase(uid: "2:5cb0f1d3-0861-41e7-a741-9bc2ba08a88b".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:85afbdc5-b6f0-405e-beda-58bb27b28fda".asUid()),
-            RoomCase(uid: "2:f6ed3557-94f5-439a-b140-0fae522dfc5d".asUid()),
-            RoomCase(uid: "2:d84dcfee-49db-4f39-962f-dc04e62037c8".asUid()),
+            RoomCase(uid: "3:d0800022-fc53-4d53-9b42-74fa1181366f".asUid()),
+            RoomCase(uid: "3:4b76d77a-0b29-4aa5-b042-44193b6662bd".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
+            RoomCase(uid: "3:a7c9605b-79cd-4764-a235-65edcecc6b2e".asUid()),
+            RoomCase(uid: "3:5b53a194-f9ea-48c4-9653-bab6f8b637ff".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
+            RoomCase(uid: "3:c123a6b0-57b7-45aa-8bb4-2b9d4ac273bf".asUid()),
+            RoomCase(uid: "3:95e40d70-6586-4662-aba3-ac17d3ca8faf".asUid()),
           ],
         ),
       ),
@@ -77,14 +76,14 @@ class ShowCaseRepo {
         groupedRooms: GroupedRooms(
           name: "اخبار جدید",
           roomsList: [
-            RoomCase(uid: "2:d84dcfee-49db-4f39-962f-dc04e62037c8".asUid()),
-            RoomCase(uid: "2:85afbdc5-b6f0-405e-beda-58bb27b28fda".asUid()),
-            RoomCase(uid: "2:975a9d21-8fbf-4ab4-a264-ba07fa857119".asUid()),
-            RoomCase(uid: "2:2c6abdb2-8a6a-4a57-9dac-9aacf936835c".asUid()),
-            RoomCase(uid: "2:5cb0f1d3-0861-41e7-a741-9bc2ba08a88b".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f6ed3557-94f5-439a-b140-0fae522dfc5d".asUid()),
+            RoomCase(uid: "3:5b53a194-f9ea-48c4-9653-bab6f8b637ff".asUid()),
+            RoomCase(uid: "3:d0800022-fc53-4d53-9b42-74fa1181366f".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
+            RoomCase(uid: "3:4b76d77a-0b29-4aa5-b042-44193b6662bd".asUid()),
+            RoomCase(uid: "3:c123a6b0-57b7-45aa-8bb4-2b9d4ac273bf".asUid()),
+            RoomCase(uid: "3:95e40d70-6586-4662-aba3-ac17d3ca8faf".asUid()),
+            RoomCase(uid: "3:4b76d77a-0b29-4aa5-b042-44193b6662bd".asUid()),
+            RoomCase(uid: "3:a7c9605b-79cd-4764-a235-65edcecc6b2e".asUid()),
           ],
         ),
       ),
@@ -101,14 +100,14 @@ class ShowCaseRepo {
         groupedRooms: GroupedRooms(
           name: "علم روز",
           roomsList: [
-            RoomCase(uid: "2:975a9d21-8fbf-4ab4-a264-ba07fa857119".asUid()),
-            RoomCase(uid: "2:2c6abdb2-8a6a-4a57-9dac-9aacf936835c".asUid()),
-            RoomCase(uid: "2:5cb0f1d3-0861-41e7-a741-9bc2ba08a88b".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f8e51f85-0e31-4d5b-94f2-bf5ed37b1e66".asUid()),
-            RoomCase(uid: "2:f6ed3557-94f5-439a-b140-0fae522dfc5d".asUid()),
-            RoomCase(uid: "2:d84dcfee-49db-4f39-962f-dc04e62037c8".asUid()),
-            RoomCase(uid: "2:85afbdc5-b6f0-405e-beda-58bb27b28fda".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
+            RoomCase(uid: "3:c123a6b0-57b7-45aa-8bb4-2b9d4ac273bf".asUid()),
+            RoomCase(uid: "3:95e40d70-6586-4662-aba3-ac17d3ca8faf".asUid()),
+            RoomCase(uid: "3:a7c9605b-79cd-4764-a235-65edcecc6b2e".asUid()),
+            RoomCase(uid: "3:5b53a194-f9ea-48c4-9653-bab6f8b637ff".asUid()),
+            RoomCase(uid: "3:d0800022-fc53-4d53-9b42-74fa1181366f".asUid()),
+            RoomCase(uid: "3:4b76d77a-0b29-4aa5-b042-44193b6662bd".asUid()),
+            RoomCase(uid: "3:edb16986-ee0a-4e07-a474-29344f27af5a".asUid()),
           ],
         ),
       ),
