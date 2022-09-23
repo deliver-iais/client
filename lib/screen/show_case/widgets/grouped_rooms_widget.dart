f0a7a2ff2373db103cc217a84c7b7578047bc09b
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-09-23 13:18:53 +0330

    add AllGroupedRoomsGridPage

--- lib/screen/show_case/widgets/grouped_rooms_widget.dart
+++ lib/screen/show_case/widgets/grouped_rooms_widget.dart
@@ -1,51 +1,49 @@
-import 'package:deliver/localization/i18n.dart';
-import 'package:deliver/repository/roomRepo.dart';
-import 'package:deliver/shared/widgets/circle_avatar.dart';
-import 'package:deliver/shared/widgets/room_name.dart';
+import 'package:deliver/screen/show_case/widgets/grouped_rooms_item_widget.dart';
+import 'package:deliver/services/routing_service.dart';
 import 'package:deliver_public_protocol/pub/v1/models/showcase.pb.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 class GroupedRoomsWidget extends StatelessWidget {
+  static final _routingService = GetIt.I.get<RoutingService>();
   final GroupedRooms groupedRooms;
-  static final _roomRepo = GetIt.I.get<RoomRepo>();
-  static String _roomName = "";
-  static final _i18n = GetIt.I.get<I18N>();
 
   const GroupedRoomsWidget({Key? key, required this.groupedRooms})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Column(
+    return Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
                   groupedRooms.name,
                   style: const TextStyle(
                     fontWeight: FontWeight.w600,
                     fontSize: 16,
                   ),
                 ),
-                const Icon(Icons.arrow_forward),
-              ],
-            ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_forward),
+                onPressed: () => _routingService.openAllGroupedRoomsGridPage(
+                  groupedRooms: groupedRooms,
+                ),
+              ),
+            ],
           ),
           _buildGroupedRoomsList()
         ],
-      ),
     );
   }
 
   Widget _buildGroupedRoomsList() {
     return SizedBox(
-      height: 150,
+      height: 130,
       child: ListView.builder(
         itemCount: groupedRooms.roomsList.length,
         scrollDirection: Axis.horizontal,
@@ -54,32 +52,7 @@ class GroupedRoomsWidget extends StatelessWidget {
             width: 100,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatarWidget(
-                    groupedRooms.roomsList[index].uid,
-                    40,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  FutureBuilder<String>(
-                    initialData: _roomRepo.fastForwardName(
-                      groupedRooms.roomsList[index].uid,
-                    ),
-                    future:
-                        _roomRepo.getName(groupedRooms.roomsList[index].uid),
-                    builder: (context, snapshot) {
-                      _roomName = snapshot.data ?? _i18n.get("loading");
-                      return RoomName(
-                        uid: groupedRooms.roomsList[index].uid,
-                        name: _roomName,
-                      );
-                    },
-                  ),
-                ],
-              ),
+              child: GroupedRoomsItem(uid: groupedRooms.roomsList[index].uid),
             ),
           );
         },
