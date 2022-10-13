af3bfa9dab4dbe450be905ec88d49ddb0467804e
hasan <bitbeter@gmail.com>
2022-10-13 06:37:39 +0330

    minor changes

--- lib/shared/loaders/text_loader.dart
+++ lib/shared/loaders/text_loader.dart
@@ -1,3 +1,4 @@
+import 'package:deliver/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -5,8 +6,9 @@ class TextLoader extends StatelessWidget {
   final Text text;
 
   final double width;
+  final BorderRadius? borderRadius;
 
-  const TextLoader(this.text, {super.key, this.width = 80});
+  const TextLoader(this.text, {super.key, this.width = 80, this.borderRadius});
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +23,12 @@ class TextLoader extends StatelessWidget {
       baseColor: theme.outline.withOpacity(0.15),
       highlightColor: theme.onSurface.withOpacity(0.23),
       child: Container(
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          color: theme.surface,
+        ),
         width: width,
         height: (text.style?.fontSize ?? 10) + 2,
-        color: theme.surface,
       ),
     );
   }
