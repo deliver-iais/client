368b4c1d9f185259f6323c20cf920af7fc71d002
Fatemeh Chitsaz <f.chitsaz7919@gmail.com>
2022-10-28 13:44:11 +0330

    change text selection for input message

---
+++ lib/screen/room/messageWidgets/custom_text_selection/methods/custom_text_selection_methods.dart
@@ -0,0 +1,209 @@
+import 'package:deliver/localization/i18n.dart';
+import 'package:deliver/screen/room/widgets/auto_direction_text_input/auto_direction_text_form.dart';
+import 'package:deliver/services/raw_keyboard_service.dart';
+import 'package:deliver/shared/parsers/parsers.dart';
+import 'package:deliver_public_protocol/pub/v1/models/uid.pb.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class CustomTextSelectionMethods {
+  static final _i18n = GetIt.I.get<I18N>();
+  static final _rawKeyboardService = GetIt.I.get<RawKeyboardService>();
+
+  static void moveCursor(
+    TextSelectionDelegate delegate,
+    int offset,
+    TextEditingController textController,
+  ) {
+    delegate.hideToolbar();
+    textController.selection = TextSelection.collapsed(
+      offset: offset,
+    );
+  }
+
+  static void desktopPastHandle(
+    TextSelectionDelegate delegate,
+    TextEditingController textController,
+    Uid roomUid,
+    BuildContext buildContext,
+  ) {
+    _rawKeyboardService.controlVHandle(
+      textController,
+      buildContext,
+      roomUid,
+    );
+    delegate.hideToolbar();
+  }
+
+  static void handleFormatting(
+    TextSelectionDelegate delegate,
+    String specialChar,
+    TextEditingController textController,
+  ) {
+    if (isAnyThingSelected(textController)) {
+      final end = textController.selection.end;
+      textController.text = createFormattedText(specialChar, textController);
+      moveCursor(
+        delegate,
+        end + specialChar.length * 2,
+        textController,
+      );
+    }
+  }
+
+  static bool isAnyThingSelected(TextEditingController textController) {
+    final start = textController.selection.start;
+    final end = textController.selection.end;
+    if (start != end &&
+        textController.text.substring(start, end).trim().isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static void handleCreateLink(
+    TextSelectionDelegate delegate,
+    BuildContext buildContext,
+    TextEditingController textController,
+  ) {
+    final formKey = GlobalKey<FormState>();
+    final linkTextController = TextEditingController();
+    final linkController = TextEditingController();
+    final end = textController.selection.end;
+    final start = textController.selection.start;
+
+    if (isAnyThingSelected(textController)) {
+      linkTextController.text = textController.text.substring(start, end);
+    }
+    showDialog(
+      context: buildContext,
+      builder: (context) {
+        return AlertDialog(
+          actionsPadding: const EdgeInsets.only(bottom: 8, right: 8),
+          content: SizedBox(
+            width: 200,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _i18n.get("create_link"),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      createLinkTextField(
+                        linkTextController,
+                        _i18n.get("text"),
+                      ),
+                      const SizedBox(height: 10),
+                      createLinkTextField(
+                        linkController,
+                        _i18n.get("link"),
+                        useLinkValidator: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                _i18n.get("cancel"),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                final isValidated = formKey.currentState?.validate() ?? false;
+                if (isValidated) {
+                  final link =
+                      createLink(linkTextController.text, linkController.text);
+
+                  textController.text = textController.text.substring(
+                        0,
+                        start,
+                      ) +
+                      link +
+                      textController.text.substring(
+                        isAnyThingSelected(textController) ? end : start,
+                      );
+
+                  Navigator.pop(context);
+                }
+              },
+              child: Text(
+                _i18n.get("create"),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static Widget createLinkTextField(
+    TextEditingController controller,
+    String label, {
+    bool useLinkValidator = false,
+  }) {
+    return Directionality(
+      textDirection: _i18n.defaultTextDirection,
+      child: AutoDirectionTextForm(
+        controller: controller,
+        validator: useLinkValidator ? validateLink : validateTextLink,
+        decoration: InputDecoration(
+          labelText: label,
+          contentPadding: const EdgeInsets.only(),
+          border: const UnderlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  static String? validateLink(String? value) {
+    final urlRegex = RegExp(UrlFeature.urlRegex);
+    final inlineUrlRegex = RegExp(UrlFeature.inlineUrlRegex);
+    if (value!.isEmpty) {
+      return null;
+    } else if (!urlRegex.hasMatch(value) && !inlineUrlRegex.hasMatch(value)) {
+      return _i18n.get("link_valid");
+    }
+    return null;
+  }
+
+  static String? validateTextLink(String? value) {
+    if (value == null) return null;
+    if (value.isEmpty) {
+      return _i18n.get("text_empty");
+    } else {
+      return null;
+    }
+  }
+
+  static String createLink(String text, String link) {
+    return "[$text]($link)";
+  }
+
+  static String createFormattedText(
+    String specialChar,
+    TextEditingController textController,
+  ) {
+    return "${textController.text.substring(0, textController.selection.start)}"
+        "$specialChar${textController.text.substring(textController.selection.start, textController.selection.end)}"
+        "$specialChar${textController.text.substring(textController.selection.end, textController.text.length)}";
+  }
+}
