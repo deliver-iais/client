91d00943b5a133158610a5328a45c48723298b14
hasan <bitbeter@gmail.com>
2022-11-23 07:06:14 +0330

    minor changes

--- lib/shared/changelog.dart
+++ lib/shared/changelog.dart
@@ -2,7 +2,6 @@
 
 const List<String> FARSI_FEATURE_LIST = [
   'رفع مشکلات ارسال فایل در پیام رسان',
-  'حذف مجوز اضافی ACTIVITY_RECOGNITION',
   'اضافه شدن Reply با پیام صوتی',
   'ازین پس شما میتوانید موقعیت مکانی به غیر از موقعیت خود ارسال نمایید',
   'رفع برخی باگ‌های تماس صوتی و تصویری',
@@ -11,7 +10,6 @@ const List<String> FARSI_FEATURE_LIST = [
 
 const List<String> ENGLISH_FEATURE_LIST = [
   'Some bugs fixed in sending file',
-  'Removing extra ACTIVITY_RECOGNITION permission',
   'Now you can send reply with voice',
   'Now you can send other location than your location',
   'Voice call and Video call bugs fixed',
