b659b7fc29e71074b1e00846d56081ec916f082d
Fatemeh Chitsaz <f.chitsaz7918@gmail.com>
2022-12-12 08:01:40 +0330

    WIP

--- lib/screen/profile/widgets/operation_on_media.dart
+++ lib/screen/profile/widgets/operation_on_media.dart
@@ -2,6 +2,7 @@ import 'package:deliver/box/message.dart';
 import 'package:deliver/localization/i18n.dart';
 import 'package:deliver/models/operation_on_message.dart';
 import 'package:deliver/screen/room/pages/build_message_box.dart';
+import 'package:deliver/shared/methods/platform.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -30,78 +31,98 @@ class OperationOnImageState extends State<OperationOnMedia> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return Directionality(
       textDirection: _i18n.defaultTextDirection,
       child: Column(
         children: [
-          PopupMenuItem(
-            onTap: () async {
-              final message = await (widget.getMessage());
-              return OperationOnMessageSelection(
-                message: message!,
-                context: context,
-              ).selectOperation(
-                OperationOnMessage.SHOW_IN_FOLDER,
-              );
-            },
-            child: Row(
-              children: [
-                const Icon(CupertinoIcons.folder_open),
-                const SizedBox(width: 8),
-                Text(
-                  _i18n.get("show_in_folder"),
-                  style: theme.primaryTextTheme.bodyText2,
-                ),
-              ],
+          if (isWindows || isLinux || isMacOS) ...[
+            _buildPopupMenuItem(
+              _i18n.get("show_in_folder"),
+              CupertinoIcons.folder_open,
+              () async {
+                final message = await (widget.getMessage());
+                return OperationOnMessageSelection(
+                  message: message!,
+                  context: context,
+                ).selectOperation(
+                  OperationOnMessage.SHOW_IN_FOLDER,
+                );
+              },
             ),
+            _buildPopupMenuItem(
+              _i18n.get("save_to_downloads"),
+              CupertinoIcons.down_arrow,
+              () async {
+                final message = await (widget.getMessage());
+                return OperationOnMessageSelection(
+                  message: message!,
+                  context: context,
+                ).selectOperation(
+                  OperationOnMessage.SAVE_TO_DOWNLOADS,
+                );
+              },
+            ),
+            _buildPopupMenuItem(
+              _i18n.get("save_as"),
+              Icons.save_alt_rounded,
+              () async {
+                final message = await (widget.getMessage());
+                return OperationOnMessageSelection(
+                  message: message!,
+                  context: context,
+                ).selectOperation(
+                  OperationOnMessage.SAVE_AS,
+                );
+              },
+            ),
+          ],
+          if (isAndroid) ...[
+            _buildPopupMenuItem(
+              _i18n.get("share"),
+              Icons.share_rounded,
+              () async {
+                final message = await (widget.getMessage());
+                return OperationOnMessageSelection(
+                  message: message!,
+                  context: context,
+                ).selectOperation(
+                  OperationOnMessage.SHARE,
+                );
+              },
+            ),
+            _buildPopupMenuItem(
+              _i18n.get("save_to_gallery"),
+              CupertinoIcons.down_arrow,
+              () async {
+                final message = await (widget.getMessage());
+                return OperationOnMessageSelection(
+                  message: message!,
+                  context: context,
+                ).selectOperation(
+                  OperationOnMessage.SAVE_TO_GALLERY,
+                );
+              },
+            ),
+          ]
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPopupMenuItem(String text, IconData icon, VoidCallback onTap) {
+    return PopupMenuItem(
+      padding: EdgeInsets.zero,
+      onTap: () => onTap(),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(icon, color: Colors.white, size: 18),
           ),
-          PopupMenuItem(
-            onTap: () async {
-              final message = await (widget.getMessage());
-              return OperationOnMessageSelection(
-                message: message!,
-                context: context,
-              ).selectOperation(
-                OperationOnMessage.SAVE_TO_DOWNLOADS,
-              );
-            },
-            child: Row(
-              children: [
-                const Icon(CupertinoIcons.down_arrow),
-                const SizedBox(width: 8),
-                Text(
-                  _i18n.get("save_to_downloads"),
-                  style: theme.primaryTextTheme.bodyText2,
-                )
-              ],
-            ),
+          Text(
+            text,
+            style: const TextStyle(color: Colors.white),
           ),
-          PopupMenuItem(
-            onTap: () async {
-              final message = await (widget.getMessage());
-              return OperationOnMessageSelection(
-                message: message!,
-                context: context,
-              ).selectOperation(
-                OperationOnMessage.SAVE_AS,
-              );
-            },
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.save_alt_rounded,
-                  size: 18,
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  _i18n.get("save_as"),
-                  style: theme.primaryTextTheme.bodyText2,
-                ),
-              ],
-            ),
-          )
         ],
       ),
     );
