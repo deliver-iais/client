328ef6ac176e141dce1e735bd95a8b18d00d2cee
Edansi <e.dansi.111@gmail.com>
2023-01-08 17:12:55 +0330

    add shared

--- lib/repository/authRepo.dart
+++ lib/repository/authRepo.dart
@@ -20,6 +20,7 @@ import 'package:grpc/grpc.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:logger/logger.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:synchronized/synchronized.dart';
 
 class AuthRepo {
@@ -48,9 +49,11 @@ class AuthRepo {
 
   Future<void> init({bool retry = false}) async {
     try {
+      final prefs = await SharedPreferences.getInstance();
+      final accessToken = prefs.getString(SHARED_DAO_ACCESS_TOKEN_KEY) ?? "";
+      final refreshToken = prefs.getString(SHARED_DAO_REFRESH_TOKEN_KEY) ?? "";
       _localPassword = await _sharedDao.get(SHARED_DAO_LOCAL_PASSWORD) ?? "";
-      final accessToken = await _sharedDao.get(SHARED_DAO_ACCESS_TOKEN_KEY);
-      final refreshToken = await _sharedDao.get(SHARED_DAO_REFRESH_TOKEN_KEY);
+
       return _setTokensAndCurrentUserUid(accessToken, refreshToken);
     } catch (e) {
       _logger.e(e);
