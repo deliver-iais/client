9bb36dd3747bf481bfa3f11d7adc28f1c7c5e128
hasan <bitbeter@gmail.com>
2023-01-29 10:39:31 +0330

    chore

--- lib/screen/room/widgets/bot_start_information_box_widget.dart
+++ lib/screen/room/widgets/bot_start_information_box_widget.dart
@@ -40,139 +40,145 @@ class BotStartInformationBoxWidget extends StatelessWidget {
     return FutureBuilder<BotInfo?>(
       future: _botRepo.getBotInfo(roomUid),
       builder: (c, botInfo) {
-        final showBox=botInfo.hasData &&
+        final showBox = botInfo.hasData &&
             botInfo.data != null &&
             botInfo.data!.description != null &&
             botInfo.data!.description!.isNotEmpty;
-          final botCommands = botInfo.data?.commands;
-          final botDescription = botInfo.data?.description;
-          return  PageTransitionSwitcher(
-              transitionBuilder: (
-                  child,
-                  animation,
-                  secondaryAnimation,
-                  ) {
-                return FadeThroughTransition(
-                  fillColor: Colors.transparent,
-                  animation: animation,
-                  secondaryAnimation: secondaryAnimation,
-                  child: child,
-                );
-              },
-            child: showBox ? Container(
-              decoration: const BoxDecoration(
-                boxShadow: DEFAULT_BOX_SHADOWS,
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              ),
-              child: ClipRRect(
-                clipBehavior: Clip.hardEdge,
-                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: Material(
-                    color: Colors.transparent,
+        final botCommands = botInfo.data?.commands;
+        final botDescription = botInfo.data?.description;
+        return PageTransitionSwitcher(
+          transitionBuilder: (
+            child,
+            animation,
+            secondaryAnimation,
+          ) {
+            return FadeThroughTransition(
+              fillColor: Colors.transparent,
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              child: child,
+            );
+          },
+          child: showBox
+              ? Container(
+                  decoration: const BoxDecoration(
+                    boxShadow: DEFAULT_BOX_SHADOWS,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: ClipRRect(
                     clipBehavior: Clip.hardEdge,
-                    elevation: 1.0,
-                    type: MaterialType.card,
-                    child: Container(
-                      width: 300,
-                      constraints: const BoxConstraints(maxHeight: 500),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            theme.colorScheme.primary.withOpacity(0.3),
-                            backGroundColors[0].withOpacity(0.3),
-                            backGroundColors[1].withOpacity(0.3),
-                            theme.colorScheme.tertiary.withOpacity(0.3)
-                          ],
-                        ),
-                      ),
-                      child: SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                botDescription!,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                ),
-                                textDirection: _i18n.getDirection(botDescription),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              ListView.separated(
-                                padding: EdgeInsets.zero,
-                                shrinkWrap: true,
-                                itemCount: botCommands!.length,
-                                itemBuilder: (c, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 8,
-                                      vertical: 8,
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: Material(
+                        color: Colors.transparent,
+                        clipBehavior: Clip.hardEdge,
+                        elevation: 1.0,
+                        type: MaterialType.card,
+                        child: Container(
+                          width: 300,
+                          constraints: const BoxConstraints(maxHeight: 500),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                theme.colorScheme.primary.withOpacity(0.3),
+                                backGroundColors[0].withOpacity(0.3),
+                                backGroundColors[1].withOpacity(0.3),
+                                theme.colorScheme.tertiary.withOpacity(0.3)
+                              ],
+                            ),
+                          ),
+                          child: SingleChildScrollView(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    botDescription!,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
                                     ),
-                                    child: MouseRegion(
-                                      cursor: SystemMouseCursors.click,
-                                      child: GestureDetector(
-                                        behavior: HitTestBehavior.translucent,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Flexible(
-                                              child: Text(
-                                                "/${botCommands.keys.toList()[index]}",
-                                                style: TextStyle(
-                                                  color:
-                                                      theme.colorScheme.primary,
-                                                  fontSize: 16,
-                                                ),
-                                              ),
-                                            ),
-                                            const SizedBox(width: 10),
-                                            Expanded(
-                                              child: Text(
-                                                botCommands.values
-                                                    .toList()[index],
-                                                textDirection: TextDirection.rtl,
-                                                style: TextStyle(
-                                                  color: Colors.white
-                                                      .withOpacity(0.8),
-                                                ),
-                                              ),
-                                            ),
-                                          ],
+                                    textDirection:
+                                        _i18n.getDirection(botDescription),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  ListView.separated(
+                                    padding: EdgeInsets.zero,
+                                    shrinkWrap: true,
+                                    itemCount: botCommands!.length,
+                                    itemBuilder: (c, index) {
+                                      return Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 8,
+                                          vertical: 8,
                                         ),
-                                        onTap: () {
-                                          _messageRepo.sendTextMessage(
-                                            roomUid,
-                                            "/${botCommands.keys.toList()[index]}",
-                                          );
-                                        },
-                                      ),
+                                        child: MouseRegion(
+                                          cursor: SystemMouseCursors.click,
+                                          child: GestureDetector(
+                                            behavior:
+                                                HitTestBehavior.translucent,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Flexible(
+                                                  child: Text(
+                                                    "/${botCommands.keys.toList()[index]}",
+                                                    style: TextStyle(
+                                                      color: theme
+                                                          .colorScheme.primary,
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ),
+                                                const SizedBox(width: 10),
+                                                Expanded(
+                                                  child: Text(
+                                                    botCommands.values
+                                                        .toList()[index],
+                                                    textDirection:
+                                                        TextDirection.rtl,
+                                                    style: TextStyle(
+                                                      color: Colors.white
+                                                          .withOpacity(0.8),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            onTap: () {
+                                              _messageRepo.sendTextMessage(
+                                                roomUid,
+                                                "/${botCommands.keys.toList()[index]}",
+                                              );
+                                            },
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    separatorBuilder: (context, index) =>
+                                        Divider(
+                                      color: Colors.white.withOpacity(0.2),
                                     ),
-                                  );
-                                },
-                                separatorBuilder: (context, index) => Divider(
-                                  color: Colors.white.withOpacity(0.2),
-                                ),
-                              )
-                            ],
+                                  )
+                                ],
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ):const SizedBox.shrink(),
-          );
-
+                )
+              : const SizedBox.shrink(),
+        );
       },
     );
   }
