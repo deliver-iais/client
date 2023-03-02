91927daf3b11b001557d87ff5bc9116ac516048d
h.beheshti <bitbeter@gmail.com>
2023-03-02 11:42:46 +0330

    update camera looks

--- lib/screen/room/widgets/share_box/cemara_box.dart
+++ lib/screen/room/widgets/share_box/cemara_box.dart
@@ -4,6 +4,8 @@ import 'package:deliver/repository/messageRepo.dart';
 import 'package:deliver/screen/room/widgets/share_box/open_image_page.dart';
 import 'package:deliver/screen/room/widgets/share_box/video_viewer_page.dart';
 import 'package:deliver/services/camera_service.dart';
+import 'package:deliver/shared/constants.dart';
+import 'package:deliver/shared/methods/colors.dart';
 import 'package:deliver/shared/methods/format_duration.dart';
 import 'package:deliver/shared/widgets/blurred_container.dart';
 import 'package:deliver_public_protocol/pub/v1/models/uid.pb.dart';
@@ -39,6 +41,8 @@ class _CameraBoxState extends State<CameraBox> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Stack(
       children: [
         StreamBuilder<bool>(
@@ -63,28 +67,31 @@ class _CameraBoxState extends State<CameraBox> {
                 duration.data! > 0) {
               return Align(
                 alignment: Alignment.topCenter,
-                child: SizedBox(
-                  width: 100,
-                  child: BlurContainer(
-                    skew: 4,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          CupertinoIcons.video_camera,
-                          color: Colors.red,
-                          size: 40,
-                        ),
-                        DefaultTextStyle(
-                          style: const TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 16,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 100,
+                    child: BlurContainer(
+                      skew: 10,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            CupertinoIcons.video_camera,
+                            color: Colors.red,
+                            size: 40,
                           ),
-                          child: Text(
-                            formatDuration(Duration(seconds: duration.data!)),
+                          DefaultTextStyle(
+                            style: const TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 16,
+                            ),
+                            child: Text(
+                              formatDuration(Duration(seconds: duration.data!)),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -95,75 +102,116 @@ class _CameraBoxState extends State<CameraBox> {
         ),
         Align(
           alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 30),
-            child: GestureDetector(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+          child: Expanded(
+            child: Container(
+              color: theme.colorScheme.surface.withOpacity(0.5),
+              padding: const EdgeInsets.symmetric(
+                vertical: 24.0,
+                horizontal: 24.0,
+              ),
+              height: 180,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Icon(
-                    CupertinoIcons.circle,
-                    size: 80,
-                  ),
-                  if (!widget.selectAsAvatar)
-                    DefaultTextStyle(
-                      style: const TextStyle(decoration: TextDecoration.none),
-                      child: Text(
-                        _i18n.get("take_picture_and_video_helper"),
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: getEnableBackgroundColor(
+                      isEnable: _cameraService.enableAudio(),
+                    ),
+                    child: IconButton(
+                      onPressed: () async {
+                        await _cameraService.changeRecordAudioState();
+                        setState(() {});
+                      },
+                      icon: getEnableIcon(
+                        isEnable: _cameraService.enableAudio(),
+                        enableIcon: CupertinoIcons.volume_up,
+                        disableIcon: CupertinoIcons.volume_mute,
+                        size: 25,
                       ),
                     ),
+                  ),
+                  StreamBuilder<int>(
+                    stream: _cameraService.getDuration(),
+                    builder: (context, snapshot) {
+                      final isRecording = (snapshot.data ?? 0) > 0;
+                      return GestureDetector(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            AnimatedContainer(
+                              duration: ANIMATION_DURATION,
+                              height: isRecording ? 85 : 75,
+                              width: isRecording ? 85 : 75,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color:
+                                      isRecording ? Colors.red : Colors.white,
+                                  width: 6,
+                                ),
+                                color: isRecording
+                                    ? Colors.red
+                                    : Colors.transparent,
+                              ),
+                            ),
+                            if (!widget.selectAsAvatar) ...[
+                              const SizedBox(height: 12),
+                              DefaultTextStyle(
+                                style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: isRecording ? 0 : null,
+                                  color:
+                                      isRecording ? Colors.transparent : null,
+                                ),
+                                child: Text(
+                                  _i18n.get("take_picture_and_video_helper"),
+                                ),
+                              ),
+                            ],
+                          ],
+                        ),
+                        onTap: () => _cameraService.takePicture().then((file) {
+                          if (widget.selectAsAvatar) {
+                            Navigator.pop(context);
+                            widget.onAvatarSelected!(file.path);
+                          } else {
+                            openImage(file);
+                          }
+                        }),
+                        onLongPressStart: (_) => !widget.selectAsAvatar
+                            ? _cameraService.startVideoRecorder()
+                            : null,
+                        onLongPressEnd: (d) => !widget.selectAsAvatar
+                            ? _onRouteToVideoViewer()
+                            : null,
+                      );
+                    },
+                  ),
+                  if (_cameraService.hasMultiCamera())
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor:
+                          getEnableBackgroundColor(isEnable: false),
+                      child: IconButton(
+                        onPressed: () => _cameraService.switchToAnotherCamera(),
+                        icon: const Icon(
+                          CupertinoIcons.camera_rotate,
+                        ),
+                        color: getEnableColor(isEnable: false),
+                        iconSize: 25,
+                      ),
+                    )
+                  else
+                    const SizedBox(
+                      width: 50,
+                      height: 50,
+                    )
                 ],
               ),
-              onTap: () => _cameraService.takePicture().then((file) {
-                if (widget.selectAsAvatar) {
-                  Navigator.pop(context);
-                  widget.onAvatarSelected!(file.path);
-                } else {
-                  openImage(file);
-                }
-              }),
-              onLongPressStart: (_) => !widget.selectAsAvatar
-                  ? _cameraService.startVideoRecorder()
-                  : null,
-              onLongPressEnd: (d) =>
-                  !widget.selectAsAvatar ? _onRouteToVideoViewer() : null,
             ),
           ),
         ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 30, left: 10),
-            child: IconButton(
-              onPressed: () async {
-                await _cameraService.changeRecordAudioState();
-                setState(() {});
-              },
-              icon: Icon(
-                _cameraService.enableAudio()
-                    ? CupertinoIcons.volume_mute
-                    : CupertinoIcons.volume_up,
-              ),
-              color: Colors.white70,
-              iconSize: 35,
-            ),
-          ),
-        ),
-        if (_cameraService.hasMultiCamera())
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 30, right: 10),
-              child: IconButton(
-                onPressed: () => _cameraService.switchToAnotherCamera(),
-                icon: const Icon(
-                  CupertinoIcons.arrow_2_squarepath,
-                ),
-                color: Colors.white70,
-                iconSize: 35,
-              ),
-            ),
-          )
       ],
     );
   }
