1a2135ec375d4fc28dba3b33d15ab2d2362709bc
h.beheshti <bitbeter@gmail.com>
2023-03-05 11:06:08 +0330

    update changelogs

--- lib/shared/changelog.dart
+++ lib/shared/changelog.dart
@@ -3,21 +3,25 @@
 const List<String> FARSI_FEATURE_LIST = [
   'اضافه کردن قابلیت انتخاب میکروفن در دسکتاپ برای تماس صوتی و تصویری',
   'بهبود نمایش نوتیفیکیشن‌های برنامه در اندروید و مک',
+  'اضافه کردن قابلیت ضبط ویدیو در خود کلاینت‌های موبایلی',
+  'اضافه شدن تنظیمات بیشتر برای اندازه فونت، کشیدگی متن، پخش صدا ارسال و دریافت پیام در هنگام چت',
   'رفع برخی باگ‌ها در سینک سین',
   'رفع برخی باگ‌ها در نمایش markupها در پیام‌های بات',
   'رفع باگ نمایش درصد دانلود فایل در نسخه وب',
   'رفع برخی باگ‌های بازارسال پیام‌ها از گروه و کانال‌ها',
   'رفع مشکل مهم پیام‌های تکراری در پیام رسان',
-  'و رفع برخی باگ‌های دیگر',
+  'و رفع برخی باگ‌های گزارش شده‌ی دیگر',
 ];
 
 const List<String> ENGLISH_FEATURE_LIST = [
   'Add microphone setting for desktop in video and voice calls',
   'Better implementation of showing notifications in Android and MacOS',
+  'Add recording video feature in mobile apps',
+  'Add more settings for font size, justification and in-chat sounds',
   'Some bugs fixed in seen synchronization',
   'Some bugs fixed in message markups UI',
   'Some bugs fixed in showing percents of downloading file in web',
   'Some bugs fixed in forwarding message',
   'Some bugs fixed repeating sending same message',
-  'And some other bugs fixed...',
+  'And many reported bugs fixed...',
 ];
