1dc50e87674dd2c8fb4ffd0eed3bba53601cfe3d
h.beheshti <bitbeter@gmail.com>
2023-03-06 20:23:25 +0330

    bug fix in jfif files

--- lib/shared/methods/file_helpers.dart
+++ lib/shared/methods/file_helpers.dart
@@ -99,11 +99,10 @@ String _detectFileMimeByFilePathForWeb(String? fileName, String? filePath) {
 file_model.MimeByNameAndContent _detectFileTypeByNameAndContent(
   String? filePath,
 ) {
-  final typeByContent = lookupMimeType(
-        "no-file",
-        headerBytes: File(filePath ?? "").readAsBytesSync(),
-      ) ??
-      DEFAULT_FILE_TYPE;
+  final typeByContent = lookupMimeTypeFromPath(
+    "no-file",
+    headerBytes: File(filePath ?? "").readAsBytesSync(),
+  );
 
   return file_model.MimeByNameAndContent(
     filePath.getMimeString(),
@@ -115,11 +114,10 @@ file_model.MimeByNameAndContent _detectFileTypeByNameAndContentForWeb(
   String? fileName,
   String? filePath,
 ) {
-  final typeByContent = lookupMimeType(
-        "no-file",
-        headerBytes: _getWebFileData(filePath!),
-      ) ??
-      DEFAULT_FILE_TYPE;
+  final typeByContent = lookupMimeTypeFromPath(
+    "no-file",
+    headerBytes: _getWebFileData(filePath!),
+  );
 
   return file_model.MimeByNameAndContent(
     fileName.getMimeString(),
@@ -186,7 +184,7 @@ extension MimeTypeOfFileName on String? {
     if (this == null || this!.trim().isEmpty) {
       return DEFAULT_FILE_TYPE;
     } else {
-      return lookupMimeType(this!) ?? DEFAULT_FILE_TYPE;
+      return lookupMimeTypeFromPath(this!);
     }
   }
 
@@ -262,6 +260,10 @@ extension ImagePath on String {
     );
   }
 }
+
+String lookupMimeTypeFromPath(String path, {List<int>? headerBytes}) =>
+    lookupMimeType(path, headerBytes: headerBytes) ??
+    (path.contains("jfif") ? "image/jpeg" : DEFAULT_FILE_TYPE);
 
 // TODO(bitbeter): Use these for later
 // const List<String> videoFormats = [
