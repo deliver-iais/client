839f83712e0d686bfa3f30f0529fe7a2532fc51d
hasan <bitbeter@gmail.com>
2023-06-13 16:39:40 +0330

    update changelog

--- lib/shared/changelog.dart
+++ lib/shared/changelog.dart
@@ -1,11 +1,32 @@
-const List<String> FARSI_FEATURE_LIST = [
-  'رفع برخی باگ‌های گزارش شده و بهبود کارایی برنامه',
+const FARSI_FEATURE_LIST = <String>[
+  "بازطراحی صفحه‌ی اصلی برنامه",
+  "اضافه شدن صفحه‌ی ویترین به برنامه",
+  "اضافه شدن ویژگی فروشگاه در برنامه (به زودی)",
+  "نمایش گروه‌بندی چت‌ها در صفحه‌ی اصلی",
+  "باز طراحی و بهبود قابلیت تماس صوتی و تصویری",
+  "اضافه شدن قابلیت لیست‌انتشار جهت ارسال پیام شخصی به چند نفر و همچنین ارسال از طریق پیامک",
+  "بهبود عملکرد ارسال فایل و افزایش حجم فایل ارسالی",
+  "حدود ۶۰ رفع باگ و اضافه شدن قابلیت‌های جدید در پیام‌رسان",
 ];
 
-const List<String> ENGLISH_FEATURE_LIST = [
-  'Some bugs fixes and Performance improvements',
+const ENGLISH_FEATURE_LIST = [
+  "Redesign Home Page",
+  "Showcase page added",
+  "Store page added",
+  "Showing rooms in groups view",
+  "Redesign and rework, voice and video call",
+  "Broadcasting private messages now available in APP, also SMS feature in broadcasts",
+  "Improvements in sending file and file size limit",
+  "About 60 other new features and bug fixes",
 ];
 
-const List<String> ARABIC_FEATURE_LIST = [
-  'Some bugs fixes and Performance improvements',
+const ARABIC_FEATURE_LIST = [
+  "Redesign Home Page",
+  "Showcase page added",
+  "Store page added",
+  "Showing rooms in groups view",
+  "Redesign and rework, voice and video call",
+  "Broadcasting private messages now available in APP, also SMS feature in broadcasts",
+  "Improvements in sending file and file size limit",
+  "About 60 other new features and bug fixes",
 ];
